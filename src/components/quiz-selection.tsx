a3a2bbcc2a447fef04250174ea43267c7e1b845b
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2024-04-02 13:49:47 -0300

    feat: add selection quiz component

---
+++ src/components/quiz-selection.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { useQuiz } from '@/context/quiz-context'
+import { QuizButton } from './quiz/quiz-button'
+import { QuizScreen } from './quiz/quiz-screen'
+
+export interface QuizProps {
+  quizzes: {
+    title: string
+    icon: string
+    questions: {
+      question: string
+      options: string[]
+      answer: string
+    }[]
+  }[]
+}
+
+interface QuizSelectionProps {
+  quizData: QuizProps
+}
+
+export function QuizSelection({ quizData }: QuizSelectionProps) {
+  const { selectedQuiz, setSelectedQuiz } = useQuiz()
+
+  if (selectedQuiz !== null) {
+    return <QuizScreen quizData={quizData} />
+  }
+
+  return (
+    <div className="grid grid-cols-2 pt-56">
+      <div className="flex flex-col gap-12">
+        <h1 className="text-white text-[64px] leading-none">
+          Welcome to the <span className="font-bold">Frontend Quiz!</span>
+        </h1>
+        <span className="text-lightBluish italic text-xl">
+          Pick a subject to get started.
+        </span>
+      </div>
+      <div className="flex flex-col gap-6">
+        {quizData.quizzes.map((quiz, i) => (
+          <QuizButton
+            key={quiz.title}
+            title={quiz.title}
+            imgUrl={quiz.icon}
+            onClick={() => setSelectedQuiz(i)}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
